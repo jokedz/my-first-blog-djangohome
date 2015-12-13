9b7d52d163ea9d3a0d3d886160b279a97ec01c19
askjalynx <askja.lynx@gmail.com>
2015-12-13 15:05:32 +0100

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,13 @@
+print('hello djangohome')
+
+def hi (name):
+	print('Witaj' + ' ' + name + '!')
+
+
+dziewczyny = ['Asia', 'Kasia', 'Justyna']
+for imie in dziewczyny:
+	hi(imie)
+	print('Kolejna dziewczyna')
+
+
+
